82866e1cd1d55dcc07f0a30cb6602e2f071ca54a
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2022-12-27 15:45:57 +0000

    added image

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ const API_KEY = "69af5f3159ff0070dad5481146157688";
 
 async function getCharacters() {
   const response = await fetch(`https://gateway.marvel.com/v1/public/characters?apikey=${API_KEY}`);
-  const data = await response.json();
-  console.log(data);
+  const data = await response.json()
+  console.log(data)
+
+  const image = `${data.data.results[7].thumbnail.path}.${data.data.results[7].thumbnail.extension}`
+
+  result.innerHTML = `<p>hero name: ${data.data.results[7].name}</p>
+                      <img src="${image}"/>` 
 }
 
-getCharacters();
+getCharacters()
